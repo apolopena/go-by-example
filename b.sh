e6e75d2a4bb21e54941821b65071b5f75c02eb83
apolopena <3060702+apolopena@users.noreply.github.com>
2021-07-03 16:14:35 -0700

    initial commit

---
+++ b.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+check() {
+  [[ ! -f $f ]] && echo "Error: $f does not exist" && exit 1
+  return 0
+}
+
+build () {
+  check "$f" && go build "$f" && mv "$(basename "$b1")" "$b2"
+}
+
+main() {
+  if [[ $1 == '-e' ]]; then
+    f="src/$2.go" && b1="${f%.*}" && b2="bin/$(basename "$b1")"
+    build && ./"$b2"
+  else
+    f="src/$1.go" && b1="${f%.*}" && b2="bin/$(basename "$b1")"
+    build
+  fi
+}
+
+main "$@"
---
+++ r.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# r.sh: run a .go file from src/
+
+go run "src/$1.go"
