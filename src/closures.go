7de62258f3c8e78db00fb3dfd624611a48f4b581
apolopena <3060702+apolopena@users.noreply.github.com>
2021-07-06 23:35:48 -0700

    first commit

---
+++ src/closures.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func main() {
+
+	nextInt := intSeq()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	newInts := intSeq()
+	fmt.Println(newInts())
+}
+
+/*
+Outputs
+1
+2
+3
+1
+*/
